a0f01d35a078b711106b3da2d9df09ae0e6019d1
anzelin <an172048@outlook.com>
2026-01-11 12:58:26 +0800

    chore: 初始化 Chrome 扩展项目基础配置

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+import { crx } from "@crxjs/vite-plugin"
+import manifest from "./manifest.json"
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    crx({ manifest }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    cors: true,
+    hmr: {
+      host: 'localhost',
+      protocol: 'ws'
+    }
+  },
+})

37f565f3cda84eab960bd1c7b0d6f37e5e1b94ad
anzelin <an172048@outlook.com>
2026-02-12 00:23:50 +0800

    refactor(cleanup): remove paper, pomodoro and todo modules

--- src/apps/paper/PaperPage.tsx
+++
@@ -1,9 +0,0 @@
-import { PaperEditor } from "./PaperEditor";
-
-export function PaperPage() {
-    return (
-        <div className="h-screen w-screen overflow-hidden">
-            <PaperEditor />
-        </div>
-    );
-}
--- src/apps/paper/index.ts
+++
@@ -1,3 +0,0 @@
-export { PaperDialog } from './PaperDialog';
-export { PaperEditor } from './PaperEditor';
-export { PaperPage } from './PaperPage';
